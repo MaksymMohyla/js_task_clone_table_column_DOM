e763ac4af40e8febbdd762dab22f0ac875258893
Maksym Mohyla <greenparksquad@gmail.com>
2024-08-30 11:56:43 +0300

    make build

---
+++ index.2eabbb72.js
@@ -0,0 +1,2 @@
+document.querySelector("table").querySelectorAll("tr").forEach(function(e){var l=e.cells[1].cloneNode(!0);e.insertBefore(l,e.lastChild.previousSibling)});
+//# sourceMappingURL=index.2eabbb72.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const table = document.querySelector("table");
+const rows = table.querySelectorAll("tr");
+rows.forEach((row)=>{
+    const positionCell = row.cells[1];
+    const newPositionCell = positionCell.cloneNode(true);
+    row.insertBefore(newPositionCell, row.lastChild.previousSibling);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
